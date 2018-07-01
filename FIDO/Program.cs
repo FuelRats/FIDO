26631638a8ea363f928bc3e30c48608563fa5b3c
Adanaran <adanaran@fuelrats.com>
2018-07-01 16:44:50 +0200

    Add solution files.

---
+++ FIDO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
