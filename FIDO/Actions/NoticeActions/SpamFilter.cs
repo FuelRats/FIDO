4c29ea2712087874356f9b42817c54723b4cfb4e
Adanaran <adanaran@fuelrats.com>
2018-09-17 17:21:14 +0200

    Add redirect of op signal to another channel if configured

--- FIDO/Actions/NoticeActions/SpamFilter.cs
+++ FIDO/Actions/NoticeActions/SpamFilter.cs
@@ -9,10 +9,16 @@ namespace FIDO.Actions.NoticeActions
   public class SpamFilter : NoticeAction
   {
     private static readonly Regex regex = new Regex(@"\[Spamfilter\] (?<nick>[A-Za-z0-9_´|\[\]]*)!(?<user>[A-Za-z0-9_´|\[\]]*)@(?<host>[A-Za-z0-9.:_\-]*) matches filter '(?<filter>.*)': \[(?<message>[A-Za-z0-9]* (?<target>[A-Za-z0-9#_`\[\]]*):.*)]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    private readonly string opsignalChannel;
 
     public SpamFilter(IrcLayer irc, NexmoClient nexmo, IConfiguration configuration)
       : base(irc, nexmo, configuration)
     {
+      opsignalChannel = configuration["OpsignalChannel"];
+      if (string.IsNullOrWhiteSpace(opsignalChannel) || !opsignalChannel.StartsWith('#'))
+      {
+        opsignalChannel = null;
+      }
     }
 
     protected override Regex Regex => regex;
@@ -23,7 +29,7 @@ namespace FIDO.Actions.NoticeActions
     {
       if (filter == "*opsignal*" || filter == "*opssignal*")
       {
-        ReportToIrc($@"{"OPSIGNAL".WrapWithColour(Colours.LightRed)} by {nick} in channel / query {target}. Original message: ""{message}""");
+        ReportToIrc($@"{"OPSIGNAL".WrapWithColour(Colours.LightRed)} by {nick} in channel / query {target}. Original message: ""{message}""", opsignalChannel);
         SendSms($"OPSIGNAL by {nick} in channel / query {target}. Original message: {message}");
       }
       else
