6a78f6365b3653d6de9b9ac160d9c60690c57b95
Kenneth Aalberg <kenneaal@gmail.com>
2021-04-10 00:55:59 +0200

    Aaand we're still dealing with a list of course...

--- modules/commands/rcjoin.py
+++ modules/commands/rcjoin.py
@@ -1,5 +1,6 @@
 from typing import List
 from config import IRC
+
 import fido
 
 from modules.access import require_permission, Levels
@@ -12,6 +13,7 @@ log = logging.getLogger(__name__)
 async def rcjoin(bot: fido, channel: str, sender: str, args: List[str]):
     if len(args) == 0:
         return "Usage: " + IRC.commandPrefix + "rcjoin <client name>"
-    if args in bot.users:
-        await bot.message(channel, f"{args} is being joined to #ratchat.")
-        await bot.raw(f"SAJOIN {args} #ratchat\n")
+    for arg in args:
+        if arg in bot.users:
+            await bot.message(channel, f"{arg} is being joined to #ratchat.")
+            await bot.raw(f"SAJOIN {arg} #ratchat\n")
