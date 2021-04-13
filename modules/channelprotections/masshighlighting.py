5d79a0de8d1790f2984a0e28ba4fb6ad9b0172a9
Kenneth Aalberg <kenneaal@gmail.com>
2021-04-13 19:42:35 +0200

    Make exact matches for nickname in highlight protection. More efficient list processing. (1.03 usec/item)

--- modules/channelprotections/masshighlighting.py
+++ modules/channelprotections/masshighlighting.py
@@ -11,12 +11,11 @@ async def on_message(bot: fido, channel: str, sender: str, message: str):
         session.query(config.Config).filter_by(module='channelprotection', key='maxhighlightcount')[0].value)
 
     highlighted_users: List[str] = []
+    channel_users = [x.lower() for x in bot.channels[channel]['users']]
     for word in message.split(' '):
-        word = word.lstrip('@')
-        for user in bot.channels[channel]['users']:
-            if word.startswith(user) and user not in highlighted_users:
-                print(f"Matched as user: {word}")
-                highlighted_users.append(user)
+        word = word.lstrip('@').lower()
+        if word in channel_users:
+            highlighted_users.append(word)
     if len(highlighted_users) >= max_highlight_count:
         await bot.kick(channel, sender, kick_reason)
         await bot.message("#opers", f"{sender} was kicked from channel {channel} for highlight spam.")
