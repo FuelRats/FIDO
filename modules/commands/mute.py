53b5b07a459c679b145de9c0bcb2fc82a808ab23
Kenneth Aalberg <kenneaal@gmail.com>
2025-03-17 00:09:20 +0100

    Add optional timed mutes.

--- modules/commands/mute.py
+++ modules/commands/mute.py
@@ -6,6 +6,7 @@ import fido
 from modules.access import require_permission, Levels
 from models import config, SessionManager
 import logging
+import re
 
 log = logging.getLogger(__name__)
 
@@ -21,17 +22,33 @@ async def invoke(bot: fido, channel: str, sender: str, args: List[str]):
     :return: Reply message.
     """
     if len(args) == 0:
-        return "Usage: " + IRC.commandPrefix + "mute <nickname>"
-    session = SessionManager().session
+        return "Usage: " + IRC.commandPrefix + "mute <x>[m|h|d] <nickname>"
+
+    time_pattern = re.compile(r'(\d+)([mhd])')
+    time_arg = args[0]
+    match = time_pattern.match(time_arg)
+
+    if match:
+        time_value = int(match.group(1))
+        time_unit = match.group(2)
+        if time_unit == 'm':
+            mutetime = time_value
+        elif time_unit == 'h':
+            mutetime = time_value * 60
+        elif time_unit == 'd':
+            mutetime = time_value * 1440
+        args = args[1:]
+    else:
+        session = SessionManager().session
+        mutetime = session.query(config.Config).filter_by(module='channelprotection',
+                                                          key='mutetime').one_or_none().value or 6
+
     log.debug(f"Processing {args}")
     for arg in args:
         if arg == "":
             continue
         if arg in bot.users:
-            mutetime = session.query(config.Config).\
-                filter_by(module='channelprotection', key='mutetime').one_or_none()
             await bot.set_mode(channel, "-v", f"{bot.users[arg]['nickname']}")
-            await bot.set_mode(channel, "+b", f"~t:{mutetime.value or 6}:~quiet:*!*@{bot.users[arg]['hostname']}")
-            # await bot.rawmsg("MODE", f":#help +b ~t:{mutetime.value or 6}:~q:*!*@{bot.users[arg]['hostname']}")
-            await bot.message(channel, f"Muted {bot.users[arg]['nickname']} for {mutetime.value or 6} minutes.")
-            log.info(f"{bot.users[arg]['nickname']} was muted for {mutetime.value or 6} minutes by {sender}")
+            await bot.set_mode(channel, "+b", f"~t:{mutetime}:~quiet:*!*@{bot.users[arg]['hostname']}")
+            await bot.message(channel, f"Muted {bot.users[arg]['nickname']} for {mutetime} minutes.")
+            log.info(f"{bot.users[arg]['nickname']} was muted for {mutetime} minutes by {sender}")
